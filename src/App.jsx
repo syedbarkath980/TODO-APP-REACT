7e39ad2d1c639181997821050476393a0014fd67
Syed <syedbarkath980@gmail.com>
2026-04-30 23:53:31 +0530

    added local storage feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { use, useEffect, useState } from "react";
 
 const App = () => {
   const [initial, setInitial] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(() => {
+    // Loads data from local storage when page re-reloads:
+    const storage = localStorage.getItem("list");
+    return storage ? JSON.parse(storage) : [];
+  });
+
+  useEffect(() => {
+    // Stores the data inside localStorage using a key called as list:
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   const handleAddBtn = () => {
     let storePrevious = [...list, initial];
@@ -10,18 +19,13 @@ const App = () => {
     setInitial("");
   };
 
+  // Filter Removes an element if the condition is false
   const handleDeleteBtn = (index) => {
     const deledtedList = list.filter((currElem, id) => {
       return id != index;
     });
     setList(deledtedList);
   };
-
-  // const handleOnchange = (index) => {
-  //   const strikedList = list.map((i, item) => {
-  //     item[i].style = {"strikethrough"}
-  //   })
-  // }
 
   return (
     <div>
@@ -39,7 +43,7 @@ const App = () => {
       <ul>
         {list.map((task, index) => (
           <li key={index}>
-            <input type="checkbox" onChange={() => handleOnchange(index)} />
+            <input type="checkbox" />
             {task}{" "}
             <button onClick={() => handleDeleteBtn(index)}>Delete</button>
           </li>
